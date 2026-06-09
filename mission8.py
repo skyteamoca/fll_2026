53a2051c8ce638d26c39973d0bc23c25ad0e8836
skyteamoca <skyteamoca@gmail.com>
2026-06-09 14:16:09 +0200

    Create mission8.py

---
+++ mission8.py
@@ -0,0 +1,12 @@
+
+
+def misja_m8():
+    wait(100)
+    robot_move.jazda_prosto(420, predkosc_max=800)
+    wait(50)
+    for _ in range(2):
+        base.c_motor.run_angle(1000, 130)
+        wait(50)
+        base.c_motor.run_angle(-1000, 130)
+        wait(50)
+    robot_move.jazda_prosto(-450, predkosc_max=800)
