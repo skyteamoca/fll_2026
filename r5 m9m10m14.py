e1c8088b81995376292d126b58507ff5618a5589
skyteamoca <skyteamoca@gmail.com>
2026-06-09 14:14:00 +0200

    Update r5 m9m10m14.py

--- r5 m9m10m14.py
+++ r5 m9m10m14.py
@@ -1,3 +1,28 @@
 import base
 import robot_move
 from pybricks.tools import wait
+
+def misja_m9m10m14():
+    wait(100)
+    base.c_motor.run_angle(500, -1000, wait=False)
+    robot_move.jazda_prosto(280, predkosc_max=700, pauza=10)
+    robot_move.spin_turn_v1(-105, predkosc_max=500, pauza=10)
+    robot_move.jazda_prosto(250, predkosc_max=700, pauza=10)
+    
+    # skręt do artefaktu
+    base.c_motor.run_angle(500, 1000, wait=False)
+    robot_move.spin_turn_v1(40, predkosc_max=800, pauza=10)
+    
+    # wyciąganie artefaktu
+    robot_move.jazda_prosto(115, predkosc_max=800, pauza=100)
+    base.c_motor.run_angle(500, -1000)
+    wait(10)
+    base.prawy.run_time(600, 2500, wait=False)
+    base.lewy.run_time(545, 2500)
+    wait(10)
+    base.prawy.run_angle(-500, 115)
+    wait(10)
+    base.d_motor.run_angle(50, 200)
+    wait(10)
+    robot_move.jazda_prosto(-20, predkosc_max=200, pauza=10)
+    robot_move.spin_turn_small(10, 50)
