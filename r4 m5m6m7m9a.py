98a390e1b65f2bae3d48cf31a7eecfdc15940441
skyteamoca <skyteamoca@gmail.com>
2026-06-09 14:13:23 +0200

    Update r4 m5m6m7m9a.py

--- r4 m5m6m7m9a.py
+++ r4 m5m6m7m9a.py
@@ -1,3 +1,17 @@
 import base
 import robot_move
 from pybricks.tools import wait
+
+def misja_m5m6m7m9a():
+    wait(10)
+    base.c_motor.run_angle(-1000, 200, wait=False)
+    robot_move.jazda_prosto(675, predkosc_max=800)
+    wait(10)
+    robot_move.spin_turn_v1(50, predkosc_max=800, Kp=7)
+    wait(10)
+    robot_move.spin_turn_small(-13, predkosc_max=500)
+    wait(10)
+    base.c_motor.run_angle(-1000, 200)
+    wait(10)
+    robot_move.jazda_prosto(32, predkosc_max=100)
+    wait(10)
