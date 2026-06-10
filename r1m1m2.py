c323830639ce92e92005d761d2382152254f1f12
skyteamoca <skyteamoca@gmail.com>
2026-06-10 19:44:29 +0200

    Update r1m1m2.py

--- r1m1m2.py
+++ r1m1m2.py
@@ -1,20 +1,26 @@
-#Matt 34
+#Matt
 import base
 import robot_move
 from pybricks.tools import wait
 
 def run():
+    #move forward for mission 1
     robot_move.jazda_prosto(700, predkosc_max=800, dystans_hamowania=125, pauza=50)
     robot_move.jazda_prosto(-210, predkosc_max=800, pauza=50)
-    robot_move.nakladka_lewa(93, -200)
+    #about to catch the artifact in m1
+    robot_move.nakladka_lewa(84, -200)
     robot_move.jazda_prosto(80, predkosc_max=750, pauza=50)
+    #lifts the artifact
     robot_move.nakladka_lewa(100, 100)
+    #going for mission 2
     robot_move.jazda_prosto(80, predkosc_max=500, pauza=10)
-    robot_move.spin_turn_small(-41, 260, pauza=10)
+    robot_move.spin_turn_small(-40.5, 260, pauza=10)
     robot_move.jazda_prosto(25, predkosc_max=800, pauza=10)
+    #catching the artfact in m2
     robot_move.nakladka_prawa(-185, predkosc=600, pauza=10)
     robot_move.spin_turn_small(4, 100, pauza=50)
     robot_move.jazda_prosto(165, predkosc_max=500, pauza=10)
     robot_move.spin_turn_small(7, 100, pauza=10)
     robot_move.spin_turn_small(-10, 100, pauza=10)
+    #going back to the base
     robot_move.smooth_turn(-550, 100, 500)
