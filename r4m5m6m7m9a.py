248ecd4f1387864e8eb2e20f8802f793a075b37f
skyteamoca <skyteamoca@gmail.com>
2026-06-10 19:28:54 +0200

    Refactor robot movement commands in r4m5m6m7m9a.py
    Adjusted motor angles and added new movements for the robot.

--- r4m5m6m7m9a.py
+++ r4m5m6m7m9a.py
@@ -11,7 +11,27 @@ def run():
     wait(10)
     robot_move.spin_turn_small(-13, predkosc_max=500)
     wait(10)
-    base.c_motor.run_angle(-1000, 200)
+    base.c_motor.run_angle(-1000, 175)#200
     wait(10)
     robot_move.jazda_prosto(32, predkosc_max=100)
     wait(10)
+    #podniesienie dźwigu
+    base.c_motor.run_angle(350, 350)#375
+    wait(10)
+    robot_move.jazda_prosto(-80, predkosc_max=500)
+    wait(10)
+    
+    # skręt do stołu
+    robot_move.spin_turn_v1(-130, predkosc_max=500)
+    wait(100)
+    base.prawy.run_time(-500, 1100, wait=False)
+    base.lewy.run_time(-415, 1100)
+    wait(50)
+    base.prawy.run_time(500, 1000, wait=False)
+    base.lewy.run_time(180, 1000)
+    robot_move.jazda_prosto(575, predkosc_max=600)
+    wait(100)
+    robot_move.spin_turn_v1(125, predkosc_max=500)
+    wait(100)
+    base.d_motor.run_angle(40, -142)
+    # robot_move.jazda_prosto(-50, predkosc_max=600) 
