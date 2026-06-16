0801dd6f6a56f348ea9e151aea2cb95614f8fa70
skyteamoca <skyteamoca@gmail.com>
2026-06-16 18:32:51 +0200

    Adjust robot movement parameters in run function

--- r4m5m6m7m9a.py
+++ r4m5m6m7m9a.py
@@ -5,11 +5,11 @@ from pybricks.tools import wait
 def run():
     wait(10)
     base.c_motor.run_angle(-1000, 200, wait=False)
-    robot_move.jazda_prosto(688, predkosc_max=800)
+    robot_move.jazda_prosto(692, predkosc_max=800)
     wait(10)
-    robot_move.spin_turn_v1(-19, predkosc_max=600, Kp=7, pauza=10)
+    robot_move.spin_turn_v1(-20, predkosc_max=200, Kp=7, pauza=100)
     #wyrzucenie głazów
-    robot_move.spin_turn_v1(61, predkosc_max=800, Kp=7)
+    robot_move.spin_turn_v1(64, predkosc_max=500, Kp=7)
     wait(10)
     #robot_move.spin_turn_small(-13, predkosc_max=500)
     #wait(10)
@@ -26,8 +26,8 @@ def run():
     #wypchanie kamieni
     robot_move.spin_turn_v1(-129, predkosc_max=500)
     wait(100)
-    base.prawy.run_time(-500, 1100, wait=False)
-    base.lewy.run_time(-415, 1100)
+    base.prawy.run_time(-500, 1200, wait=False)
+    base.lewy.run_time(-415, 1200)
     wait(50)
     #Silos
     #robot_move.nakladka_lewa(-90, 500)
@@ -37,7 +37,7 @@ def run():
     robot_move.nakladka_lewa(-90, 400)
     robot_move.nakladka_lewa(90, 2000)
     robot_move.nakladka_lewa(-90, 400)
-    robot_move.smooth_turn(600, 100, 500, pauza=10)
+    robot_move.smooth_turn(300, 180, 500, pauza=10)
 
     #Podjazd do wagonika
     # base.prawy.run_time(500, 1000, wait=False)
