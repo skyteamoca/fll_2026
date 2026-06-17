611d53e7127b9d1a1231b4e509baa4f787e429f5
skyteamoca <skyteamoca@gmail.com>
2026-06-17 18:58:55 +0200

    Update r1m1m2.py

--- r1m1m2.py
+++ r1m1m2.py
@@ -4,11 +4,12 @@ import robot_move
 from pybricks.tools import wait
 
 def run():
+    base.d_motor.run_until_stalled(speed=200, duty_limit=55)
     #move forward for mission 1
     robot_move.jazda_prosto(700, predkosc_max=800, dystans_hamowania=125, pauza=50)
     robot_move.jazda_prosto(-210, predkosc_max=800, pauza=50)
     #about to catch the artifact in m1
-    robot_move.nakladka_lewa(84, -200)
+    robot_move.nakladka_lewa(91, -200)
     robot_move.jazda_prosto(80, predkosc_max=750, pauza=50)
     #lifts the artifact
     robot_move.nakladka_lewa(100, 100)
@@ -18,8 +19,9 @@ def run():
     robot_move.jazda_prosto(25, predkosc_max=800, pauza=10)
     #catching the artfact in m2
     robot_move.nakladka_prawa(-185, predkosc=600, pauza=10)
+    #base.c_motor.run_until_stalled(speed=-600, duty_limit=70)
     robot_move.spin_turn_small(4, 100, pauza=50)
-    robot_move.jazda_prosto(165, predkosc_max=500, pauza=10)
+    robot_move.jazda_prosto(167, predkosc_max=500, pauza=10)
     robot_move.spin_turn_small(7, 100, pauza=10)
     robot_move.spin_turn_small(-10, 100, pauza=10)
     #going back to the base
