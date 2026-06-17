ee46fd74b4994b7f34879837541d0b17ba156f5f
skyteamoca <skyteamoca@gmail.com>
2026-06-17 13:31:02 +0200

    Refactor movement angles and wait times in run function

--- r4m5m6m7m9a.py
+++ r4m5m6m7m9a.py
@@ -1,21 +1,25 @@
+#Matt 17.06
 import base
 import robot_move
 from pybricks.tools import wait
 
 def run():
     wait(10)
-    base.c_motor.run_angle(-1000, 200, wait=False)
+    base.c_motor.run_angle(-1000, 190, wait=False)
     robot_move.jazda_prosto(692, predkosc_max=800)
     wait(10)
+    #skret do stołu
     robot_move.spin_turn_v1(-20, predkosc_max=200, Kp=7, pauza=100)
     #wyrzucenie głazów
     robot_move.spin_turn_v1(63, predkosc_max=200, Kp=7)
-    wait(50)
+    wait(20)
     robot_move.spin_turn_small(-3, predkosc_max=500)
-    wait(50)
-    base.c_motor.run_angle(-1000, 175)#200
+    wait(20)
+    robot_move.jazda_prosto(-10, predkosc_max=100)
+    wait(20)
+    base.c_motor.run_angle(-1000, 185)#200
     wait(10)
-    robot_move.jazda_prosto(16, predkosc_max=100)
+    robot_move.jazda_prosto(26, predkosc_max=100)
     wait(10)
     #podniesienie dźwigu
     base.c_motor.run_angle(350, 350)#375
@@ -26,8 +30,8 @@ def run():
     #wypchanie kamieni
     robot_move.spin_turn_v1(-129, predkosc_max=500)
     wait(100)
-    base.prawy.run_time(-480, 1150, wait=False)
-    base.lewy.run_time(-415, 1150)
+    base.prawy.run_time(-460, 1188, wait=False)
+    base.lewy.run_time(-415, 1188)
     wait(50)
     #Silos
     #robot_move.nakladka_lewa(-90, 500)
@@ -37,6 +41,8 @@ def run():
     robot_move.nakladka_lewa(-90, 400)
     robot_move.nakladka_lewa(90, 2000)
     robot_move.nakladka_lewa(-90, 400)
+    wait(10)
+    robot_move.jazda_prosto(250, predkosc_max=1200)
     #robot_move.smooth_turn(300, 180, 500, pauza=10)
 
     #Podjazd do wagonika
