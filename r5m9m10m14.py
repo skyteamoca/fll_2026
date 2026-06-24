d7e1e978735fe93d9859a9eac0ffc29aac02bfee
skyteamoca <skyteamoca@gmail.com>
2026-06-24 19:44:42 +0200

    Update robot movement parameters and comments

--- r5m9m10m14.py
+++ r5m9m10m14.py
@@ -1,4 +1,4 @@
-#Krycha 23.06
+#Krycha 24.06
 import base
 import robot_move
 from pybricks.tools import wait
@@ -27,27 +27,27 @@ def run():
     # wait(10)
     # robot_move.jazda_prosto(-20, predkosc_max=200, pauza=10)
     # robot_move.spin_turn_small(10, 50)
-    wait(500)
+
     robot_move.jazda_prosto(460, predkosc_max=1100, dystans_hamowania=100, pauza=30)
-    robot_move.spin_turn_v1(-125, predkosc_max=600,pauza=10)
+    robot_move.spin_turn_v1(-125, predkosc_max=400,pauza=50)
     robot_move.jazda_prosto(260, predkosc_max=1100, pauza=10)#stragan
-    robot_move.spin_turn_small(30,predkosc_max=600)
-    robot_move.jazda_prosto(-10, 500)
+    robot_move.spin_turn_small(33,predkosc_max=400)
+    robot_move.jazda_prosto(-10, 1000)
     #zabranie beczki
 
     robot_move.nakladka_prawa(-360, 500)#wyjęcie beczki
-    robot_move.spin_turn_v2(-35, 100, pauza=10)
-    robot_move.jazda_prosto(120, predkosc_max=700)
-    robot_move.spin_turn_v1(37, predkosc_max=600, pauza=10)
+    robot_move.spin_turn_v1(-35, 400, pauza=10)
+    robot_move.jazda_prosto(120, predkosc_max=900)
+    robot_move.spin_turn_v1(37, predkosc_max=400, pauza=10)
     robot_move.jazda_prosto(380, 1200, pauza=10)
-    robot_move.spin_turn_v1(44, predkosc_max=600, pauza=10)
-    robot_move.jazda_prosto(90, predkosc_max=600, pauza=10)
+    robot_move.spin_turn_v1(44, predkosc_max=400, pauza=10)
+    robot_move.jazda_prosto(90, predkosc_max=1100, pauza=10)
     #puszczenie beczki
-    robot_move.spin_turn_v2(-5, 700)
-    robot_move.nakladka_prawa(300, predkosc=600)
+    robot_move.spin_turn_v1(-5, 400)
+    robot_move.nakladka_prawa(300, predkosc=800)
     robot_move.spin_turn_v1(-45, 400, pauza=10)
     robot_move.jazda_prosto(580, 1000)
-    robot_move.spin_turn_v1(133, 700, pauza=20)
+    robot_move.spin_turn_v1(133, 600, pauza=20)
     robot_move.jazda_prosto(300, 1000, pauza=50)
     robot_move.jazda_prosto(-500, 1000)
 
