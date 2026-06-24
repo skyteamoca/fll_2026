2647e4cf63856c91394f4c991938ccc05cf1eed7
skyteamoca <skyteamoca@gmail.com>
2026-06-24 19:47:22 +0200

    Update r1m1m2.py

--- r1m1m2.py
+++ r1m1m2.py
@@ -1,4 +1,4 @@
-#Franek, 22.06.2026r
+#Oksana 24.06.2026r.
 import base
 import robot_move
 from pybricks.tools import wait
@@ -10,22 +10,17 @@ def run():
     robot_move.jazda_prosto(-210, predkosc_max=800, pauza=50)
     #about to catch the artifact in m1
     robot_move.nakladka_lewa(87, -200)
-    robot_move.jazda_prosto(80, predkosc_max=750, pauza=50)
+    robot_move.jazda_prosto(90, predkosc_max=750, pauza=50)
     #lifts the artifact
     robot_move.nakladka_lewa(87, 100)
     #going for mission 2
-    robot_move.jazda_prosto(100, predkosc_max=500, pauza=10)
-    robot_move.spin_turn_small(-40.5, 260, pauza=10)
-    robot_move.jazda_prosto(165, predkosc_max=800, pauza=10)
-    robot_move.spin_turn_small(2, 500, pauza=10)
-    robot_move.spin_turn_small(-2, 500, pauza=10 )
+    robot_move.jazda_prosto(110, predkosc_max=500, pauza=10)
+    robot_move.spin_turn_small(-40, 260, pauza=10)
+    robot_move.jazda_prosto(160, predkosc_max=800, pauza=10)
+    robot_move.spin_turn_small(8, 500, pauza=10)
+    robot_move.spin_turn_small(-8, 500, pauza=10 )
     robot_move.jazda_prosto(-50, 500, pauza=50)
-    # #catching the artfact in m2
-    robot_move.nakladka_prawa(-180, predkosc=600, pauza=10)
-    # #base.c_motor.run_until_stalled(speed=-600, duty_limit=70)
-    # robot_move.spin_turn_small(4, 100, pauza=50)
-    # robot_move.jazda_prosto(167, predkosc_max=500, pauza=10)
-    # robot_move.spin_turn_small(7, 100, pauza=10)
-    # robot_move.spin_turn_small(-10, 100, pauza=10)
+    # # catching the artfact in m2
+    robot_move.nakladka_prawa(-140, predkosc=600, pauza=10)
     # #going back to the base
     robot_move.smooth_turn(-550, 100, 500)
