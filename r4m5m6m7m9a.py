9068f07c17323de0bf89713d0dc345e20e89c5ec
skyteamoca <skyteamoca@gmail.com>
2026-06-25 16:42:39 +0200

    Update r4m5m6m7m9a.py

--- r4m5m6m7m9a.py
+++ r4m5m6m7m9a.py
@@ -1,4 +1,4 @@
-#Orest 25.06.26
+#KOrest 25.06
 import base
 import robot_move
 from pybricks.tools import wait
@@ -15,11 +15,11 @@ def run():
     wait(20)
     robot_move.spin_turn_small(-3, predkosc_max=500)
     wait(20)
-    robot_move.jazda_prosto(-13, predkosc_max=100)
+    robot_move.jazda_prosto(-33, predkosc_max=100)
     wait(20)
     base.c_motor.run_angle(-1000, 185)#200
     wait(10)
-    robot_move.jazda_prosto(26, predkosc_max=100)
+    robot_move.jazda_prosto(46, predkosc_max=100)
     wait(10)
     #podniesienie dźwigu
     base.c_motor.run_angle(350, 350)#375
