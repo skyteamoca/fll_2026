cc01bebe3dce0980e77dd18a33cd6a4ec15d8a06
skyteamoca <skyteamoca@gmail.com>
2026-07-03 15:40:15 +0200

    Update robot movement sequence in r5m9m10m14.py
    Refactor robot movement commands and remove commented code.

--- r5m9m10m14.py
+++ r5m9m10m14.py
@@ -1,65 +1,29 @@
-#Orest 30.06.26
+#Greg&Matt 03.07
 import base
 import robot_move
 from pybricks.tools import wait
 
 def run():
-    # wait(100)
-    # base.c_motor.run_angle(500, -1000, wait=False)
-    # robot_move.jazda_prosto(280, predkosc_max=700, pauza=10)
-    # robot_move.spin_turn_v1(-105, predkosc_max=500, pauza=10)
-    # robot_move.jazda_prosto(250, predkosc_max=700, pauza=10)
-    
-    # # skręt do artefaktu
-    # base.c_motor.run_angle(500, 1000, wait=False)
-    # robot_move.spin_turn_v1(40, predkosc_max=800, pauza=10)
-    
-    # # wyciąganie artefaktu
-    # robot_move.jazda_prosto(115, predkosc_max=800, pauza=100)
-    # base.c_motor.run_angle(500, -1000)
-    # wait(10)
-    # base.prawy.run_time(600, 2500, wait=False)
-    # base.lewy.run_time(545, 2500)
-    # wait(10)
-    # base.prawy.run_angle(-500, 115)
-    # wait(10)
-    # base.d_motor.run_angle(50, 200)
-    # wait(10)
-    # robot_move.jazda_prosto(-20, predkosc_max=200, pauza=10)
-    # robot_move.spin_turn_small(10, 50)
 
     robot_move.jazda_prosto(460, predkosc_max=1100, dystans_hamowania=100, pauza=30)
     robot_move.spin_turn_v1(-125, predkosc_max=400,pauza=50)
     robot_move.jazda_prosto(260, predkosc_max=1100, pauza=10)#stragan
-    robot_move.spin_turn_small(33,predkosc_max=400)
+    robot_move.spin_turn_v1(33,predkosc_max=400)
     robot_move.jazda_prosto(-10, 1000)
     #zabranie beczki
 
     robot_move.nakladka_prawa(-360, 500)#wyjęcie beczki
     robot_move.spin_turn_v1(-35, 400, pauza=10)
-    robot_move.jazda_prosto(230, predkosc_max=900)
-    robot_move.smooth_turn(-350,200,700)
-
-
-    # robot_move.jazda_prosto(120, predkosc_max=900)
-    # robot_move.spin_turn_v1(37, predkosc_max=400, pauza=10)
-    # robot_move.jazda_prosto(380, 1200, pauza=10)
-    # robot_move.spin_turn_v1(44, predkosc_max=400, pauza=10)
-    # robot_move.jazda_prosto(90, predkosc_max=1100, pauza=10)
-    # #puszczenie beczki
-    # robot_move.spin_turn_v1(-5, 400)
-    # robot_move.nakladka_prawa(300, predkosc=800)
-    # robot_move.spin_turn_v1(-45, 400, pauza=10)
-    # robot_move.jazda_prosto(580, 1000)
-    # robot_move.spin_turn_v1(133, 600, pauza=20)
-    # robot_move.jazda_prosto(300, 1000, pauza=50)
-    # robot_move.jazda_prosto(-500, 1000)
-
-
-
-
-
-    # robot_move.spin_turn_v1(-50, predkosc_max=700)
-    # robot_move.smooth_turn(20000, -20)
-    # base.prawy.run_angle(-60, 180)
-    # robot_move.jazda_prosto(400, 700)
+    robot_move.jazda_prosto(120, predkosc_max=900)
+    robot_move.spin_turn_v1(37, predkosc_max=400, pauza=10)
+    robot_move.jazda_prosto(380, 1200, pauza=10)
+    #turn_seal
+    robot_move.spin_turn_v1(38, predkosc_max=400, pauza=10)
+    robot_move.jazda_prosto(90, predkosc_max=1100, pauza=10)
+    #puszczenie beczki
+    robot_move.spin_turn_v1(-5, 400)
+    #wyjęcie nakładki z beczki
+    robot_move.nakladka_prawa(300, predkosc=800)
+    #podjazd do przodu i opuszczenie nakładki z artefaktami
+    robot_move.jazda_prosto(100, 300, pauza=50)
+    robot_move.nakladka_lewa(90, 200)
